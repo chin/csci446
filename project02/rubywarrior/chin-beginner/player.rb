741a3e9d91334b19790f1dbb79b53929b20e175f
Chin <cisaache@mines.edu>
2012-01-25 23:54:51 -0700

    level 6 compled of rubywarrior

--- project02/rubywarrior/chin-beginner/player.rb
+++ project02/rubywarrior/chin-beginner/player.rb
@@ -1,8 +1,14 @@
 class Player
 	@prev_health
-	
+	@forward_march = 0	
 	def play_turn(warrior)
-		if warrior.health < 20
+		if warrior.health < 9 
+			if @prev_health > warrior.health
+				warrior.walk!(:backward)
+			else
+				warrior.rest!
+			end
+		elsif warrior.health < 20
 			if @prev_health > warrior.health
 				if warrior.feel.empty?				
 					warrior.walk!
@@ -15,19 +21,26 @@ class Player
 				warrior.rest!
 			end
 		else
-			
-			if warrior.feel(:backward).empty?				
-				warrior.walk(:backward)!
-			elsif warrior.feel(:backward).captive?
-				warrior.rescue!(:backward)
+			unless @forward_march ==1 
+				if warrior.feel(:backward).wall?
+					@forward_march = 1
+				else
+					if warrior.feel(:backward).empty?				
+						warrior.walk!(:backward)
+					elsif warrior.feel(:backward).captive?
+						warrior.rescue!(:backward)
+					else
+						warrior.attack!(:backward)
+					end
+				end
 			else
-				warrior.attack!(:backward)
-			if warrior.feel.empty?				
-				warrior.walk!
-			elsif warrior.feel.captive?
-				warrior.rescue!
-			else
-				warrior.attack!
+				if warrior.feel.empty?				
+					warrior.walk!
+				elsif warrior.feel.captive?
+					warrior.rescue!
+				else
+					warrior.attack!
+				end
 			end
 		end
 		@prev_health = warrior.health
