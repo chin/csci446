8a2f6885b4f49138bd4328399868abfc98656c43
Chin <cisaache@mines.edu>
2012-01-25 18:06:07 -0700

    completed level 3 of rubywarrior

--- project02/rubywarrior/chin-beginner/player.rb
+++ project02/rubywarrior/chin-beginner/player.rb
@@ -1,7 +1,15 @@
 class Player
+	@previous_health
 	def play_turn(warrior)
-		if warrior.feel.empty?
-			warrior.walk!
+		@previous_health = warrior.health
+		if @previous_health > warrior.health
+			warrior.rest!
+		elsif warrior.feel.empty?
+			if warrior.health != 20
+				warrior.rest!
+			else
+				warrior.walk!
+			end
 		else
 			warrior.attack!
 		end
