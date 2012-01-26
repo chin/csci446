ddc606b22dd77ef35d9a534e13ffeea82ccdff59
Chin <cisaache@mines.edu>
2012-01-25 17:07:31 -0700

    adding proj3 depot files

---
+++ project03/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '842880d8d74eedc75ac19bd54c44be54aa47368dcb6c1ac64a5a7ce7ce63aadf878b03f2791bab709760aa675382e12e7f99f396b9dd8942075ec19ada5a7a5a'
