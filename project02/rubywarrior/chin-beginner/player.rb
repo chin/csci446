55d41ef5698eb0f6a9d00235fc736cb6f639b73d
Chin <cisaache@mines.edu>
2012-01-25 18:50:02 -0700

    still trucking, logic error to fix

--- project02/rubywarrior/chin-beginner/player.rb
+++ project02/rubywarrior/chin-beginner/player.rb
@@ -2,15 +2,21 @@ class Player
 	@prev_health
 	
 	def play_turn(warrior)
-		if  @prev_health > warrior.health
-			warrior.attack!
-		elsif warrior.feel.empty?
-			if warrior.health != 20
-				warrior.rest!
+		if warrior.feel.empty?
+			if warrior.health < 20
+				if @prev_health >= warrior.health
+                                        warrior.walk!
+                                else
+					warrior.rest!
+				end
 			else
 				warrior.walk!
 			end
 		else
+			#if  warrior.health < 20
+                        #	if @prev_health > warrior.health
+                         #       	warrior.attack!
+                        #	end
 			warrior.attack!
 		end
 		@prev_health = warrior.health
