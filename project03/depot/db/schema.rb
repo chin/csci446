7fc171a62c695b677549aaced87e2a0543a94f33
Chin <cisaache@mines.edu>
2012-02-06 16:40:27 -0700

    ...errors compounding... dislike, going to office hours

--- project03/depot/db/schema.rb
+++ project03/depot/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120206045156) do
+ActiveRecord::Schema.define(:version => 20120206231656) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(:version => 20120206045156) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "hased_password"
+    t.string   "salt"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
 end
