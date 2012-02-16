b5b18eb69b2ab7163031287f8d9323fb177dcfa1
Chin <cisaache@mines.edu>
2012-02-16 16:10:06 -0700

    meaningful commit message to show that i started the articles assignment

---
+++ project04/articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = 'f975e1f5490ae3b57f8020850f149fc6d4e944662b9a6bcff37b0dd025a3b4206c13545af15517657e506a8cffde0a2fa3d79cea217179f4e7317f5fc82c984c'
