5d2dd6a7c4d3a662fc37f0bae4d98eb858a28a48
Chin <cisaache@mines.edu>
2012-02-16 18:14:44 -0700

    i got most of the functionality done but have still have to do the redirections

--- project04/app/controllers/articles_controller.rb
+++ project04/app/controllers/articles_controller.rb
@@ -41,6 +41,8 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(params[:article])
+    @article.n_edited = 0
+    @article.date = Time.current.to_datetime
 
     respond_to do |format|
       if @article.save
@@ -57,7 +59,8 @@ class ArticlesController < ApplicationController
   # PUT /articles/1.json
   def update
     @article = Article.find(params[:id])
-
+    @article.n_edited += 1
+    
     respond_to do |format|
       if @article.update_attributes(params[:article])
         format.html { redirect_to @article, :notice => 'Article was successfully updated.' }
