df2f1a1fded1d155f828ebecc78571bf07924aaf
Chin <cisaache@mines.edu>
2012-02-20 23:31:24 -0700

    the update redirect is now working

--- project04/app/controllers/articles_controller.rb
+++ project04/app/controllers/articles_controller.rb
@@ -63,7 +63,7 @@ class ArticlesController < ApplicationController
     
     respond_to do |format|
       if @article.update_attributes(params[:article])
-        format.html { redirect_to @article, :notice => 'Article was successfully updated.' }
+        format.html { redirect_to articles_url, :notice => 'Article was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render :action => "edit" }
