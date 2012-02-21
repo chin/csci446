d1a000ba6713b5a650ad7e795d506319d1a4610d
Chin <cisaache@mines.edu>
2012-02-20 18:28:46 -0700

    routed the localhost to the articles main page

--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -1,5 +1,6 @@
 Articles::Application.routes.draw do
   resources :articles
+  get 'articles/index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,11 +51,12 @@ Articles::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
