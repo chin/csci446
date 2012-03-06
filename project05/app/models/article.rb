905d333087babde6c3fe9b60675a9995ab308ef1
Chin <cisaache@mines.edu>
2012-03-05 21:43:55 -0700

    gitignore added ha

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -2,5 +2,9 @@ class Article < ActiveRecord::Base
 	validates :title, :presence => true
 	validates :author, :presence => true
 	validates :body, :presence => true
-	validates_format_of :author, :without => /pat/
+	validates_format_of :author, :without => /Pat/
+
+    def self.search(page)
+      paginate :page => page, :per_page => 10
+    end
 end
