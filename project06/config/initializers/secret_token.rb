ae96086fe97e00cbbb8587b2f947f3f56e78ad85
Chin <cisaache@mines.edu>
2012-03-27 23:23:41 -0600

    started proj6 by creating the app, on forth to auth logic

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = 'c98d515d269acdf3902ca8582219b5b1fa4ebbf8dfedd9ee3718aea9fcdb56437be4e4e15722974ea88401bb7f27af4c88e2339bf7b65fc5fb64471ee0b6397e'
