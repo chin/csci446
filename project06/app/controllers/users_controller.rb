831b8824392d81dd4d66753ab03363e94afb959c
Chin <cisaache@mines.edu>
2012-03-28 00:51:52 -0600

    controller for user BAM, #autholigic = magic

--- project06/app/controllers/users_controller.rb
+++ project06/app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(:users, :notice => 'User was successfully created.') }
+        format.html { redirect_to(:users, :notice => 'Registration successfull.') }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
