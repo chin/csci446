ce44ffd42364aabad10bde78bec8841135be8a3e
Chin <cisaache@mines.edu>
2012-04-22 19:44:28 -0600

    boilercode exracted, diving in...rough couple weeks :'(

---
+++ project07/guess_the_number/main.js
@@ -0,0 +1,17 @@
+var guessesLeft = 10;
+var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+
+$(function() {
+  updateScore(guessesLeft);
+  populateHighScores(highScores);
+});
+
+function populateHighScores(scores) {
+  for (var i = 0; i < scores.length; ++i) {
+    $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
+  }
+}
+
+function updateScore(score) {
+  $('h2#score span#guessesLeft').append(score);
+}
